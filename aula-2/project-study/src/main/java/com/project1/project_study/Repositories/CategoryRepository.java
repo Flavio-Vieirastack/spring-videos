2ca25d93c0be119204497535fba93bcdbc99e67b
Flavio Vieira <flavioextreme1@hotmail.com>
2024-06-13 13:41:58 -0300

    adicionado repositorios

--- aula-2/project-study/src/main/java/com/project1/project_study/Repositories/CategoryRepository.java
+++ aula-2/project-study/src/main/java/com/project1/project_study/Repositories/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.project1.project_study.Repositories;
 
-import com.project1.project_study.Models.UserModel;
+import com.project1.project_study.Models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<UserModel, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
--- aula-2/project-study/src/main/java/com/project1/project_study/Repositories/ProductsRepository.java
+++ aula-2/project-study/src/main/java/com/project1/project_study/Repositories/ProductsRepository.java
@@ -1,9 +1,9 @@
 package com.project1.project_study.Repositories;
 
-import com.project1.project_study.Models.Category;
+import com.project1.project_study.Models.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductsRepository extends JpaRepository<Category, Long> {
+public interface ProductsRepository extends JpaRepository<Products, Long> {
 }
